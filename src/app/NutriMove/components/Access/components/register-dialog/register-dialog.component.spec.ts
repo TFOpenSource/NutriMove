56d00a0a24322229d7c532ec07ba1a7a3c793850
Jesus <jesus.cont5648@gmail.com>
2024-09-25 18:26:00 -0500

    added routing and register implementation

--- src/app/NutriMove/components/Access/components/register-dialog/register-dialog.component.spec.ts
+++ src/app/NutriMove/components/Access/components/register-dialog/register-dialog.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { RegisterDialogComponent } from './register-dialog.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('RegisterDialogComponent', () => {
+  let component: RegisterDialogComponent;
+  let fixture: ComponentFixture<RegisterDialogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
+      imports: [RegisterDialogComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(RegisterDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {ToolbarComponent} from "./NutriMove/components/Access/components/toolbar/toolbar.component";
-import {FooterComponent} from "./public/footer/footer.component";
 import {AccessPageComponent} from "./NutriMove/components/Access/pages/access-page/access-page.component";
 import {MatToolbar} from "@angular/material/toolbar";
 import {MatAnchor} from "@angular/material/button";
@@ -9,7 +8,7 @@ import {MatAnchor} from "@angular/material/button";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarComponent, FooterComponent, AccessPageComponent, MatToolbar, MatAnchor, RouterLink],
+  imports: [RouterOutlet, ToolbarComponent, AccessPageComponent, MatToolbar, MatAnchor, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
