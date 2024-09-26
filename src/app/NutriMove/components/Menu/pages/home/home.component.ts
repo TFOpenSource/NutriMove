b61e73d34325c6f38d556bf17f6ae9fd37aa6846
Jesus <jesus.cont5648@gmail.com>
2024-09-26 01:21:12 -0500

    fix toolbar home

--- src/app/NutriMove/components/Menu/pages/home/home.component.ts
+++ src/app/NutriMove/components/Menu/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from "@angular/router";
 import {MatButton} from "@angular/material/button";
 import {MatToolbar} from "@angular/material/toolbar";
+import {ToolbarComponent} from "../../../../../public/toolbar/toolbar.component";
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,8 @@ import {MatToolbar} from "@angular/material/toolbar";
     RouterOutlet,
     MatButton,
     MatToolbar,
-    RouterLink
+    RouterLink,
+    ToolbarComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
